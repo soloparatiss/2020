c263edb53cf117ebb284cdbc7e24a345b0597dbb
User Solo <soloparatiss@hotmail.com>
2020-02-20 17:27:36 -0500

    Imprime tipos de variables

--- b/vpy/ch_1/variables.py
+++ b/vpy/ch_1/variables.py
@@ -1,3 +1,8 @@
-port = 21
 banner = "FreeFloat FTP Server"
-print "[+] Checking for "+banner+" on port "+str(port)
+print type(banner)
+port = 21
+print type(port)
+portList = [21,22,80,110]
+print type(portList)
+portOpen = True
+print type(portOpen)
